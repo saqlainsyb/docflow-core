287db94ee03abe2cafc6eff8eaf7318eb26f2f00
saqlainsyb <saqlainsyb@gmail.com>
2026-03-30 00:18:25 +0530

    Awarness issue updated

--- internal/ws/client.go
+++ internal/ws/client.go
@@ -50,6 +50,7 @@ type Client struct {
 	userID     string
 	color      string // assigned cursor colour for this editing session
 	documentID string
+	lastAwareness []byte
 }
 
 // NewClient constructs a Client. Called by the WebSocket handler after upgrade.
